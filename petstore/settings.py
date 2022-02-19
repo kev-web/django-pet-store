66da67c73d5f4199860fb3f1aae4aadb5ce2c2b7
Altimann <kev-web@users.noreply.github.com>
2022-02-19 01:35:34 -0600

    added sql_mode

--- petstore/settings.py
+++ petstore/settings.py
@@ -90,7 +90,10 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT')
+        'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
