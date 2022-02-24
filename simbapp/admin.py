8fb652649761d08438c2a84b65126cd9de946d46
Altimann <kev-web@users.noreply.github.com>
2022-02-24 00:55:19 -0600

    integrated django-import-export

--- simbapp/admin.py
+++ simbapp/admin.py
@@ -1,11 +1,32 @@
+#django-import-export imports:
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+#Default Django imports
 from django.contrib import admin
-
+#My simbapp imports:
 from .models import CodingPics, DogInfo
-# Register your models here.
+
+
+
+
+#django-import-export
+#Create a class:
+class DogInfoResource(resources.ModelResource):
+    class Meta:
+        model = DogInfo
+
+
+class DogInfoAdmin(ImportExportModelAdmin):
+    resource_class = DogInfoResource
+
+
 
 
 admin.site.register(CodingPics)
-admin.site.register(DogInfo)
+admin.site.register(DogInfo, DogInfoAdmin)
+
+
+
 
 
 
